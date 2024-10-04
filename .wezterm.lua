dfea7117c12e7d41c7c4f534f45179fa315bd6fa
Term <jani.bangiev@gmail.com>
2024-10-04 10:08:14 +0200

    Update .wezterm.lua

--- .wezterm.lua
+++ .wezterm.lua
@@ -7,13 +7,33 @@ local config = wezterm.config_builder()
 -- This is where you actually apply your config choices
 
 -- For example, changing the color scheme:
-config.color_scheme = "AdventureTime"
-config.font = wezterm.font("JetBrainsMono Nerd Font Mono", { weight = "Bold", italic = false })
-config.font = wezterm.font_with_fallback({
-	{ family = "SauceCodePro NFM Medium" },
-})
+config.color_scheme = "tokyonight_moon"
+config.font = wezterm.font("JetBrainsMono Nerd Font Mono", { weight = "Bold" })
 config.font_size = 16.0
 config.default_prog = { "C:\\Program Files\\PowerShell\\7\\pwsh.exe" }
+config.window_decorations = "RESIZE"
+config.window_padding = {
+	left = 0,
+	right = 0,
+	top = 0,
+	bottom = 0,
+}
+
+config.font_rules = {
+	{
+		intensity = "Bold",
+		italic = false,
+		font = wezterm.font("JetBrainsMono Nerd Font Mono", { weight = "Bold", stretch = "Normal", style = "Normal" }),
+	},
+	{
+		intensity = "Bold",
+		italic = true,
+		font = wezterm.font("JetBrains Nerd Font Mono", { weight = "Bold", stretch = "Normal", style = "Italic" }),
+	},
+}
+
+config.window_background_opacity = 0.2
+config.win32_system_backdrop = "Acrylic"
 
 -- and finally, return the configuration to wezterm
 return config
